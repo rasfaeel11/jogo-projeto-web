d9a1bca4dee5406eddc33dbbc5a752750d3b4850
Rafael <rafaelmelo01111998@gmail.com>
2025-09-01 08:36:34 -0300

    inicio de projeto usando Spring

---
+++ src/main/java/com/rafaelMelo/rpg_cyberpunk_api/RpgCyberpunkApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelMelo.rpg_cyberpunk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgCyberpunkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgCyberpunkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelMelo/rpg_cyberpunk_api/RpgCyberpunkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelMelo.rpg_cyberpunk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgCyberpunkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
